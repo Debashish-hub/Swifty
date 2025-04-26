2a4bdc305fbe6fde6c7351b0571f7775c5c475d2
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2025-04-26 21:09:53 +0530

    Made changes for package.swift file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,29 @@
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
-    name: "Swifty"
+    name: "Swifty",
+    platforms: [
+        .iOS(.v13), // or macOS(.v10_15), etc.
+    ],
+    products: [
+        .library(
+            name: "Swifty",
+            targets: ["Swifty"]
+        ),
+    ],
+    dependencies: [
+        // (If your package depends on other libraries, add here)
+    ],
+    targets: [
+        .target(
+            name: "Swifty",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "SwiftyTests",
+            dependencies: ["Swifty"]
+        ),
+    ]
 )
+
