54d3b4954b16d1e74c6c76961027c0f6109a0760
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2025-04-27 18:52:26 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,33 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-    name: "Swifty"
+    name: "Swifty",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "Swifty",
+            targets: ["Swifty"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Swifty",
+            path: "Sources",
+            exclude: [],
+            resources: [],
+            publicHeadersPath: nil
+        ),
+        .testTarget(
+            name: "SwiftyTests",
+            dependencies: ["Swifty"],
+            path: "Tests"
+        )
+    ]
 )
 
