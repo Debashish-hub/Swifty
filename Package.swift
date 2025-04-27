dc9d57e0a240ac7792f86a0542c8e8aba399fa68
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2025-04-27 18:38:48 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,28 +2,6 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swifty",
-    platforms: [
-        .iOS(.v13), // or macOS(.v10_15), etc.
-    ],
-    products: [
-        .library(
-            name: "Swifty",
-            targets: ["Swifty"]
-        ),
-    ],
-    dependencies: [
-        // (If your package depends on other libraries, add here)
-    ],
-    targets: [
-        .target(
-            name: "Swifty",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "SwiftyTests",
-            dependencies: ["Swifty"]
-        ),
-    ]
+    name: "Swifty"
 )
 
